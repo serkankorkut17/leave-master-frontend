7dee2cfc6fa3786dfa346c3e9370d1a590db2d30
serkankorkut17 <srknkrktgs@gmail.com>
2024-05-25 17:07:52 +0300

    leave request form

--- src/pages/LeaveRequest.js
+++ src/pages/LeaveRequest.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { DateRange, DayPicker } from "react-day-picker";
-import { addDays } from "date-fns";
+import { addDays, set } from "date-fns";
 import "react-day-picker/src/style.css";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import {
+  getMaxLeaveDaysAPI,
+  createLeaveRequestAPI,
+} from "../services/LeaveRequestService";
+import { useAuthContext } from "../context/Auth";
+import Loading from "../components/Loading";
+import { toast } from "react-toastify";
+
+const validationSchema = Yup.object().shape({
+  reason: Yup.string().required().max(200),
+});
 
 const LeaveRequestPage = () => {
+  const { token } = useAuthContext();
   const initialRange = {
     // from tomorrow
     from: addDays(new Date(), 1),
@@ -14,46 +29,109 @@ const LeaveRequestPage = () => {
   const [range, setRange] = useState(initialRange);
   const [maxLeaveDays, setMaxLeaveDays] = useState(5);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = async (data) => {
+    setIsLoading(true);
+    try {
+      const response = await createLeaveRequestAPI(
+        data.reason,
+        range.from,
+        range.to
+      );
+      console.log(response);
+      if (response.data == "User already has a leave request") {
+        toast.warning("You already have a leave request pending");
+      }
+      if (response.statusText == "OK") {
+        toast.success("Leave request sent successfully");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+  };
+
   const handleSelect = (range) => {
-    // range = {
-    // "from": "2024-05-17T18:12:28.689Z",
-    // "to": "2024-05-21T21:00:00.000Z"
-    // }
-    // if range is more than maxLeaveDays, dont increase the range
-    // const diff = new Date(range.to) - new Date(range.from);
-    // const days = diff / (1000 * 60 * 60 * 24);
-    // if (days > maxLeaveDays) {
-    //   return;
-    // }
     setRange(range);
   };
 
+  useEffect(() => {
+    if (!token) return;
+    const fetchMaxLeaveDays = async () => {
+      const response = await getMaxLeaveDaysAPI();
+      // console.log(response.data);
+      setMaxLeaveDays(response.data);
+    };
+    fetchMaxLeaveDays();
+  }, [token]);
+
   return (
-    <div className="container mx-auto mt-16">
-      <h1 className="text-4xl font-bold text-center dark:text-white">
-        Leave Request Page
-      </h1>
-      <div className="mx-auto max-w-md flex justify-center items-center dark:bg-gray-800 text-center mt-5 rounded-xl">
-        <DayPicker
-          mode="range"
-          // min={1}
-          max={5}
-          selected={range}
-          onSelect={handleSelect}
-          disabled={(day) => day < new Date()}
-          className="justify-center dark:text-white"
-        />
-      </div>
-      {/* button */}
-      <div className="flex justify-center mt-5">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => console.log(range)}
-        >
-          Submit
-        </button>
-      </div>
-    </div>
+    <>
+      {isLoading && <Loading />}
+      <section className="h-full my-16 bg-gray-100 dark:bg-gray-900">
+        {/* add left top available leave days */}
+        <div className="top-4 right-4 bg-white dark:bg-gray-800 shadow-lg rounded-lg p-2 text-sm text-gray-900 dark:text-white">
+          Available Leave Days: {maxLeaveDays}
+        </div>
+        <div className="container mx-auto h-full flex justify-center items-center px-4">
+          <div className="relative top-[-50px] bg-white dark:bg-gray-800 shadow-2xl rounded-2xl p-8 md:p-12 w-full max-w-md md:max-w-lg">
+            <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-900 dark:text-white mb-6 md:mb-8">
+              Request Leave
+            </h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-4 md:mb-6">
+                <label
+                  htmlFor="reason"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Your Reason
+                </label>
+                <input
+                  type="text"
+                  id="reason"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 transition duration-200"
+                  placeholder="..."
+                  {...register("reason")}
+                  maxLength={200}
+                  required
+                />
+                {errors.reason && (
+                  <span className="text-red-500 text-sm">
+                    {errors.reason.message}
+                  </span>
+                )}
+              </div>
+              <div className="mx-auto max-w-md flex justify-center items-center dark:bg-gray-800 text-center mt-5">
+                <DayPicker
+                  mode="range"
+                  // min={1}
+                  max={maxLeaveDays}
+                  selected={range}
+                  onSelect={handleSelect}
+                  disabled={(day) => day < new Date()}
+                  className="justify-center dark:text-white"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition duration-200 mt-4"
+              >
+                Send Request
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
---
+++ src/services/LeaveRequestService.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+const api = "http://localhost:5247/api/";
+
+export const getLeaveRequestsAPI = async () => {
+  try {
+    const data = await axios.get(api + "leave-request/all");
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getLeaveRequestAPI = async () => {
+  try {
+    const data = await axios.get(api + "leave-request");
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createLeaveRequestAPI = async (reason, from, to) => {
+  try {
+    const data = await axios.post(api + "leave-request", {
+      reason: reason,
+      startDate: from,
+      endDate: to,
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+    return error.response;
+  }
+};
+
+export const deleteLeaveRequestAPI = async () => {
+  try {
+    const data = await axios.delete(api + "leave-request");
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getMaxLeaveDaysAPI = async () => {
+  try {
+    const data = await axios.get(api + "leave-request/max-leave-days");
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
